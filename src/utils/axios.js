88329356f98016e1401102defc5089fa7ac2395c
mkarabashev <mkarabashev@gmail.com>
2017-06-19 16:22:53 +0300

    backend address

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = (function () {
   switch (process.env.NODE_ENV) {
     case 'production':
-      return 'http://some-address.com'
+      return 'https://grocery-backend.herokuapp.com/'
     default:
       return 'http://localhost:3001'
   }
